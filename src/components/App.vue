f2edc8d2602d6223a42b7e66306cf635f0dfd0f6
zcy97 <1050365185@qq.com>
2024-09-18 16:19:30 +0800

    vue2-noui template commit

---
+++ src/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="rect">
+    <h1>Vue2 自定义控件</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+}
+</script>
+<style scoped>
+.rect {
+  border: 1px solid greenyellow;
+  width: 300px;
+}
+h1 {
+  color: pink;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+import path from 'path'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  let entryFile = ''
+  if (mode === 'development') {
+    entryFile = resolve(__dirname, 'src/devIndex.ts') // 开发环境入口
+  } else if (mode === 'production') {
+    entryFile = resolve(__dirname, 'src/prodIndex.ts') // 生产环境入口
+  }
+  return {
+    build: {
+      rollupOptions: {
+        input: entryFile,
+        output: {
+          entryFileNames: 'index.js', // 自定义入口文件的输出名称
+          // 对 CSS 文件以及其他静态资源的命名
+          assetFileNames: ({ name }) => {
+            if (name && name.endsWith('.css')) {
+              return 'css/index.css'
+            }
+            return 'assets/[name].[ext]'
+          },
+        },
+      },
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'), // 将 '@' 指向 'src' 目录
+      },
+    },
+  }
+})
