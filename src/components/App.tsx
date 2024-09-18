5e45b72c613aad4d75228cd92b2c54121b462ac7
zcy97 <1050365185@qq.com>
2024-09-18 16:15:13 +0800

    react-noui template commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.less'
+
+const App = (props: any) => {
+  return (
+    <div className={styles.rect}>
+      <h1>React自定义控件</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/devIndex.tsx
@@ -0,0 +1,15 @@
+/**
+ * 该文件用于开发环境实时预览效果，但无法和后端通信
+ */
+
+import ReactDOM from 'react-dom'
+import App from '@/components/App'
+
+ReactDOM.render(
+  <>
+    <div data-control-name="${CONTROL_NAME}">
+      <App />
+    </div>
+  </>,
+  document.getElementById('root') as HTMLElement
+)
