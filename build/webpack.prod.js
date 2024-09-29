cd87eb66a9d10c3284a15fd82de3cb87d26050ec
zcy97 <1050365185@qq.com>
2024-09-29 18:09:49 +0800

    build添加watch

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -5,9 +5,24 @@ const common = require('./webpack.common.js')
 const TerserPlugin = require('terser-webpack-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const WebpackBar = require('webpackbar')
+const serverConfig = require('../server/config.js')
+const { isvId, moduleId, schemaId } = serverConfig
+
+const IS_SERVER = false // 是否启用本地调试
+
+// 检查配置文件中的值是否为空
+if (IS_SERVER && (!isvId || !moduleId || !schemaId)) {
+  console.error('配置文件 server/config.js 缺少必要的字段。')
+  process.exit(1) // 退出进程
+}
 
 module.exports = merge(common, {
   mode: 'production',
+  watch: IS_SERVER,
+  watchOptions: {
+    ignored: /node_modules/, // 忽略某些目录
+  },
   optimization: {
     minimize: true,
     minimizer: [
@@ -26,7 +41,19 @@ module.exports = merge(common, {
           to: path.resolve(__dirname, '../dist'),
           noErrorOnMissing: true,
         },
+        {
+          from: path.resolve(__dirname, '../dist'),
+          to: path.join(
+            path.resolve(__dirname, '../server'),
+            'isv',
+            isvId,
+            moduleId,
+            schemaId
+          ),
+          noErrorOnMissing: true,
+        },
       ],
     }),
+    new WebpackBar(),
   ],
 })
