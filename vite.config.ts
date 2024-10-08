a00fc8d23cc9af2d95e2d54e78134dd1f643b7ff
zcy97 <1050365185@qq.com>
2024-10-08 15:22:37 +0800

    server/index update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-
+import progress from 'vite-plugin-progress'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   let entryFile = ''
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [vue()],
+    plugins: [vue(), progress()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
