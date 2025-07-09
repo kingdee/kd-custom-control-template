ad431bc15007c362c3bf224c9700e819e08d982a
zcy97 <1050365185@qq.com>
2025-07-09 10:26:20 +0800

    feta:[bos][PRJ-00588730] 自定义控件全周期优化 - 打包脚手架支持拆包配置

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@
  */
 
 import ReactDOM from 'react-dom'
-import App from '@/components/App'
+import { App } from "@/components/index";
 import '@kdcloudjs/kdesign/dist/kdesign.css'
 import eventBus from '@/utils/eventBus'
 import {
@@ -21,7 +21,7 @@ import {
   ILockUpdateProps,
   ComponentInstance,
 } from '@/types'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
 
 declare global {
   interface Window {
@@ -112,9 +112,11 @@ declare global {
           }
         }, [])
         return (
-          <div data-control-name="${CONTROL_NAME}">
-            <App model={model} customProps={newCustomProps} />
-          </div>
+          <Suspense fallback={null}>
+            <div data-control-name="${CONTROL_NAME}">
+              <App model={model} customProps={newCustomProps} />
+            </div>
+          </Suspense>
         )
       }
       ReactDOM.render(<Root model={model} customProps={customProps} />, model.dom)
